1db7ca7a28707a29939867a6e723ad1c468d9c77
Maciej Oziebly <spawek@google.com>
2020-09-30 18:11:44 +0200

    Added glob tests.

--- src/action/client_side_file_finder/action.rs
+++ src/action/client_side_file_finder/action.rs
@@ -431,6 +431,48 @@ mod tests {
     }
 
     #[test]
+    fn test_glob_star() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::create_dir(tempdir.path().join("abc")).unwrap();
+        std::fs::create_dir(tempdir.path().join("abd")).unwrap();
+        std::fs::create_dir(tempdir.path().join("xbc")).unwrap();
+
+        let request = tempdir.path().to_str().unwrap().to_owned() + "/a*c";
+        let resolved = resolve_path(&parse_path(&request));
+
+        assert_eq!(resolved.len(), 1);
+        assert_eq!(resolved[0], FsObject{path: tempdir.path().join("abc").to_str().unwrap().to_owned(), object_type: FsObjectType::Dir });
+    }
+
+    #[test]
+    fn test_glob_selection() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::create_dir(tempdir.path().join("abc")).unwrap();
+        std::fs::create_dir(tempdir.path().join("abd")).unwrap();
+        std::fs::create_dir(tempdir.path().join("xbc")).unwrap();
+
+        let request = tempdir.path().to_str().unwrap().to_owned() + "/ab[c]";
+        let resolved = resolve_path(&parse_path(&request));
+
+        assert_eq!(resolved.len(), 1);
+        assert_eq!(resolved[0], FsObject{path: tempdir.path().join("abc").to_str().unwrap().to_owned(), object_type: FsObjectType::Dir });
+    }
+
+    #[test]
+    fn test_glob_reverse_selection() {
+        let tempdir = tempfile::tempdir().unwrap();
+        std::fs::create_dir(tempdir.path().join("abc")).unwrap();
+        std::fs::create_dir(tempdir.path().join("abd")).unwrap();
+        std::fs::create_dir(tempdir.path().join("abe")).unwrap();
+
+        let request = tempdir.path().to_str().unwrap().to_owned() + "/ab[!ce]";
+        let resolved = resolve_path(&parse_path(&request));
+
+        assert_eq!(resolved.len(), 1);
+        assert_eq!(resolved[0], FsObject{path: tempdir.path().join("abd").to_str().unwrap().to_owned(), object_type: FsObjectType::Dir });
+    }
+
+    #[test]
     fn local_files_test() {
         let mut session = session::test::Fake::new();
         let request = Request{
